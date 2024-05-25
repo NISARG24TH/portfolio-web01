adcf985ab18bf1e2e60bd7c4ec89912587a14560
NISARG24TH <nisargmehra5@gmail.com>
2024-05-25 22:49:23 +0530

    updating file

--- src/page.js
+++ src/page.js
@@ -4,7 +4,7 @@ import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
-
+import AchievementsSection from "./components/AchievementsSection";
 
 export default function Home() {
   return (
